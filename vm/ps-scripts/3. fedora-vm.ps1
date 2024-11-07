a88df5592cf4a949e5ff0d79d202a786fbae2b24
luki4no <Lucian.Haralambie@me.com>
2024-11-07 12:13:24 +0200

    Update and rename create-fedora-vm.ps1 to 3. fedora-vm.ps1

--- vm/ps-scripts/3. fedora-vm.ps1
+++ vm/ps-scripts/3. fedora-vm.ps1
@@ -16,23 +16,23 @@ if (-Not (Test-Path $fedoraIsoPath)) {
 }
 
 # Create Fedora VM with specified parameters
-$fedoraVhdPath = "$diskDir\fedora.vhdx"
-New-VM -Name "fedora" -MemoryStartupBytes 4GB -NewVHDPath $fedoraVhdPath -NewVHDSizeBytes 80GB -Generation 2
-Set-VMProcessor -VMName "fedora" -Count 2
-Set-VMMemory -VMName "fedora" -DynamicMemoryEnabled $true -MinimumBytes 4GB
+$fedoraVhdPath = "$diskDir\3. fedora.vhdx"
+New-VM -Name "3. fedora" -MemoryStartupBytes 4GB -NewVHDPath $fedoraVhdPath -NewVHDSizeBytes 80GB -Generation 2
+Set-VMProcessor -VMName "3. fedora" -Count 2
+Set-VMMemory -VMName "3. fedora" -DynamicMemoryEnabled $true -MinimumBytes 4GB
 
 # Find the default network adapter and connect it to the NatSwitch
-Get-VMNetworkAdapter -VMName "fedora" | Connect-VMNetworkAdapter -SwitchName "NatSwitch"
+Get-VMNetworkAdapter -VMName "3. fedora" | Connect-VMNetworkAdapter -SwitchName "NatSwitch"
 
 # Add DVD drive to the VM and mount the Fedora ISO
-$dvdDrive = Add-VMDvdDrive -VMName "fedora" -ControllerNumber 0 -ControllerLocation 1
-Set-VMDvdDrive -VMName "fedora" -Path $fedoraIsoPath
+$dvdDrive = Add-VMDvdDrive -VMName "3. fedora" -ControllerNumber 0 -ControllerLocation 1
+Set-VMDvdDrive -VMName "3. fedora" -Path $fedoraIsoPath
 
 # Disable Secure Boot for Fedora installation
-Set-VMFirmware -VMName "fedora" -EnableSecureBoot Off
+Set-VMFirmware -VMName "3. fedora" -EnableSecureBoot Off
 
 # Set the boot order to boot from the DVD drive first
-$dvdDrive = Get-VMDvdDrive -VMName "fedora"
-Set-VMFirmware -VMName "fedora" -FirstBootDevice $dvdDrive
+$dvdDrive = Get-VMDvdDrive -VMName "3. fedora"
+Set-VMFirmware -VMName "3. fedora" -FirstBootDevice $dvdDrive
 
 Write-Host "Fedora VM has been created with the ISO mounted and the DVD drive attached."
