b1a25f6a4d8365b9d557d5cf1be672941258657a
luki4no <Lucian.Haralambie@me.com>
2024-11-07 12:16:51 +0200

    Update and rename create-kali-vm.ps1 to 5. kali-vm.ps1

--- vm/ps-scripts/5. kali-vm.ps1
+++ vm/ps-scripts/5. kali-vm.ps1
@@ -16,23 +16,23 @@ if (-Not (Test-Path $kaliIsoPath)) {
 }
 
 # Create Kali VM with specified parameters
-$kaliVhdPath = "$diskDir\kali.vhdx"
-New-VM -Name "kali" -MemoryStartupBytes 4GB -NewVHDPath $kaliVhdPath -NewVHDSizeBytes 80GB -Generation 2
-Set-VMProcessor -VMName "kali" -Count 2
-Set-VMMemory -VMName "kali" -DynamicMemoryEnabled $true -MinimumBytes 4GB
+$kaliVhdPath = "$diskDir\5. kali.vhdx"
+New-VM -Name "5. kali" -MemoryStartupBytes 4GB -NewVHDPath $kaliVhdPath -NewVHDSizeBytes 80GB -Generation 2
+Set-VMProcessor -VMName "5. kali" -Count 2
+Set-VMMemory -VMName "5. kali" -DynamicMemoryEnabled $true -MinimumBytes 4GB
 
 # Find the default network adapter and connect it to the NatSwitch
-Get-VMNetworkAdapter -VMName "kali" | Connect-VMNetworkAdapter -SwitchName "NatSwitch"
+Get-VMNetworkAdapter -VMName "5. kali" | Connect-VMNetworkAdapter -SwitchName "NatSwitch"
 
 # Add DVD drive to the VM and mount the Kali ISO
-$dvdDrive = Add-VMDvdDrive -VMName "kali" -ControllerNumber 0 -ControllerLocation 1
-Set-VMDvdDrive -VMName "kali" -Path $kaliIsoPath
+$dvdDrive = Add-VMDvdDrive -VMName "5. kali" -ControllerNumber 0 -ControllerLocation 1
+Set-VMDvdDrive -VMName "5. kali" -Path $kaliIsoPath
 
 # Disable Secure Boot for Kali installation
-Set-VMFirmware -VMName "kali" -EnableSecureBoot Off
+Set-VMFirmware -VMName "5. kali" -EnableSecureBoot Off
 
 # Set the boot order to boot from the DVD drive first
-$dvdDrive = Get-VMDvdDrive -VMName "kali"
-Set-VMFirmware -VMName "kali" -FirstBootDevice $dvdDrive
+$dvdDrive = Get-VMDvdDrive -VMName "5. kali"
+Set-VMFirmware -VMName "5. kali" -FirstBootDevice $dvdDrive
 
 Write-Host "Kali VM has been created with the ISO mounted and the DVD drive attached."
