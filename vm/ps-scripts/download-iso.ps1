62bdb2141be49b20d8b0e5504dc5ff5b19b0a606
luki4no <Lucian.Haralambie@me.com>
2024-11-11 17:58:51 +0200

    Update download-iso.ps1

--- vm/ps-scripts/download-iso.ps1
+++ vm/ps-scripts/download-iso.ps1
@@ -2,10 +2,10 @@
 $isoOption = @"
 Please select which ISO image you want to download:
 1. RedHat - CentOS (Stream-9) - Full
-2. RedHat - Fedora (41-1.4) - Server
-3. Debian - Debian (12.7.0) - NetInstaller
-4. Debian - Kali (2024.3) - NetInstaller
-5. Debian - Ubuntu (24.04.1) - Live
+2. Debian - Ubuntu (24.04.1) - Live
+3. RedHat - Fedora (41-1.4) - Server
+4. Debian - Debian (12.8.0) - NetInstaller
+5. Debian - Kali (2024.3) - NetInstaller
 "@
 
 Write-Host $isoOption
@@ -19,24 +19,24 @@ $isoUrls = @{
         File = "CentOS-Stream-9-latest-x86_64-dvd1.iso"
     }
     "2" = @{
+        Name = "Debian - Ubuntu (24.04.1) - Live"
+        Url = "https://releases.ubuntu.com/24.04.1/ubuntu-24.04.1-live-server-amd64.iso"
+        File = "ubuntu-24.04.1-live-server-amd64.iso"
+    }
+    "3" = @{
         Name = "RedHat - Fedora (41-1.4) - Server"
         Url = "https://download.fedoraproject.org/pub/fedora/linux/releases/41/Server/x86_64/iso/Fedora-Server-dvd-x86_64-41-1.4.iso"
         File = "Fedora-Server-dvd-x86_64-41-1.4.iso"
     }
-    "3" = @{
-        Name = "Debian - Debian (12.7.0) - NetInstaller"
-        Url = "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.7.0-amd64-netinst.iso"
-        File = "debian-12.7.0-amd64-DVD-1.iso"
-    }
     "4" = @{
+        Name = "Debian - Debian (12.8.0) - NetInstaller"
+        Url = "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.8.0-amd64-netinst.iso"
+        File = "debian-12.8.0-amd64-netinst.iso"
+    }
+    "5" = @{
         Name = "Debian - Kali (2024.3) - NetInstaller"
         Url = "https://cdimage.kali.org/kali-2024.3/kali-linux-2024.3-installer-netinst-amd64.iso"
         File = "kali-linux-2024.3-installer-netinst-amd64.iso"
-    }
-    "5" = @{
-        Name = "Debian - Ubuntu (24.04.1) - Live"
-        Url = "https://releases.ubuntu.com/24.04.1/ubuntu-24.04.1-live-server-amd64.iso"
-        File = "ubuntu-24.04.1-live-server-amd64.iso"
     }
 }
 
