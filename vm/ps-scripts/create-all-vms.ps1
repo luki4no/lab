705c32fcf6845dbadf0b63c65e93a393431fbb51
luki4no <Lucian.Haralambie@me.com>
2024-11-11 13:47:41 +0200

    Add files via upload

---
+++ vm/ps-scripts/create-all-vms.ps1
@@ -0,0 +1,28 @@
+# Stop the script if any error occurs
+$ErrorActionPreference = "Stop"
+
+# Path to the directory containing the VM scripts
+$scriptDir = "C:\lab\vm\ps-scripts"
+
+# Array of script names
+$scripts = @(
+    "pfsense-vm.ps1",
+    "centos-vm.ps1",
+    "ubuntu-vm.ps1",
+    "fedora-vm.ps1",
+    "debian-vm.ps1",
+    "kali-vm.ps1"
+)
+
+# Iterate through each script and execute it
+foreach ($script in $scripts) {
+    $scriptPath = Join-Path -Path $scriptDir -ChildPath $script
+    if (Test-Path $scriptPath) {
+        Write-Host "Executing $script ..."
+        & $scriptPath
+    } else {
+        Write-Host "Script $script does not exist in $scriptDir."
+    }
+}
+
+Write-Host "All VM creation scripts have been executed."
